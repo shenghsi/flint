84687b8dfd854beac97e29e0a1c49513c165cc78
sheng <shengehsi@gmail.com>
2026-07-04 07:49:43 +0800

    Fix formatting in list_bullet_item example

--- crates/ui/src/components/list/list_bullet_item.rs
+++ crates/ui/src/components/list/list_bullet_item.rs
@@ -100,7 +100,10 @@ impl Component for ListBulletItem {
                 "With Link",
                 ListBulletItem::new("")
                     .child(Label::new("Create a Flint account by"))
-                    .child(ButtonLink::new("visiting the website", "https://github.com/shenghsi/flint"))
+                    .child(ButtonLink::new(
+                        "visiting the website",
+                        "https://github.com/shenghsi/flint",
+                    ))
                     .into_any_element(),
             ),
         ];
