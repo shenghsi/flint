130f62cfa6c9508a8871031caec1a62c63a5223a
sheng <shengehsi@gmail.com>
2026-07-28 20:01:01 +0800

    text: Reduce anchor resolution overhead

--- crates/text/src/text.rs
+++ crates/text/src/text.rs
@@ -3379,7 +3379,7 @@ impl ToOffset for usize {
 impl ToOffset for Anchor {
     #[inline]
     fn to_offset(&self, snapshot: &BufferSnapshot) -> usize {
-        snapshot.summary_for_anchor(self)
+        snapshot.offset_for_anchor(self)
     }
 }
 
@@ -3521,7 +3521,7 @@ impl FromAnchor for PointUtf16 {
 impl FromAnchor for usize {
     #[inline]
     fn from_anchor(anchor: &Anchor, snapshot: &BufferSnapshot) -> Self {
-        snapshot.summary_for_anchor(anchor)
+        snapshot.offset_for_anchor(anchor)
     }
 }
 
